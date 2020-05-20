5a191559604b1f04d6887f3e0255c70773f21b7a
rmtrout42 <rmtrout42@outlook.com>
2020-05-20 10:37:52 -0400

    added HDL analysis interface

--- blood_analysis.py
+++ blood_analysis.py
@@ -1,13 +1,22 @@
 # blood_analysis.py
 def interface():
-    print("My Blood Analysis Calculator")
+    print("\nMy Blood Analysis Calculator\n")
     keep_running = True
     while keep_running:
         print("Options:")
+        print("1-HDL Analysis")
         print("9-Quit")
-        choice = input("Choose an option: ")
+        choice = input("\nChoose an option: ")
+        if choice == '1':
+            hdl_interface()
         if choice == '9':
+            print("\n")
             keep_running = False
+
+
+def hdl_interface():
+    print("HDL Interface\n")
+    pass
 
 
 if __name__ == '__main__':
