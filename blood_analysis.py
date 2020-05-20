424ab3128726b734a26c7b493bc6af1350e0d13d
rmtrout42 <rmtrout42@outlook.com>
2020-05-20 10:53:56 -0400

    added categorization of HDL input

--- blood_analysis.py
+++ blood_analysis.py
@@ -1,9 +1,32 @@
 # blood_analysis.py
+
+
+def hdl_analysis(hdl_result):
+    if hdl_result >= 60:
+        return "Good"
+    elif 40 <= hdl_result < 60:
+        return "Borderline"
+    else:
+        return "Bad"
+
+
+def hdl_interface():
+    # Input should be HDL = 66
+    print("HDL Interface\n")
+    print("Please input the result in the following format:")
+    print(" HDL=##, where ## is the numeric result.")
+    hdl_input = input("Result: ")
+    hdl_result = hdl_input.split('=')
+    print(hdl_result)
+    hdl_category = hdl_analysis(int(hdl_result[1]))
+    print(hdl_category)
+
+
 def interface():
     print("\nMy Blood Analysis Calculator\n")
     keep_running = True
     while keep_running:
-        print("Options:")
+        print("\nOptions:")
         print("1-HDL Analysis")
         print("9-Quit")
         choice = input("\nChoose an option: ")
@@ -12,11 +35,6 @@ def interface():
         if choice == '9':
             print("\n")
             keep_running = False
-
-
-def hdl_interface():
-    print("HDL Interface\n")
-    pass
 
 
 if __name__ == '__main__':
